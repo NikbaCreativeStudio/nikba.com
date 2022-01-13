95326a3278f37852d8ebb91c79aab191e0f1dfcc
Bargan Nicolai <office@nikba.com>
2022-01-14 00:33:36 +0200

    Services

--- src/pages/Service/Service.js
+++ src/pages/Service/Service.js
@@ -1,8 +1,9 @@
-import React, { Fragment } from "react";
+import React, { Fragment } from "react"
+import './Service.css'
 
 import { useStaleSWR } from '../../api'
 
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { Close } from "../../components/Close/Close";
 import { Loading } from "../../components/Loader/Loader";
 
@@ -23,7 +24,16 @@ export const Service = () => {
                     <Close Path="/services" />
 
                     <h2 className="page_title">{data.data.title}</h2>
-                    <div dangerouslySetInnerHTML={{ __html: data.data.text }} className="page_text" />
+                    <div className="service_content">
+                        <div dangerouslySetInnerHTML={{ __html: data.data.text }} className="service_text" />
+                        {data.data.features && 
+                            <div dangerouslySetInnerHTML={{__html: data.data.features}} className="service_features" />
+                        }
+                    </div>
+                    <div className="service_cta">
+                        <Link to="/works" className="quote">Recent Works</Link>
+                        <Link to="/contact/quote" className="quote">Get a Quote</Link>
+                    </div>
                 </article>
             </div>
         </Fragment>
